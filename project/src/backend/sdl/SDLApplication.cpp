222f388bb5ac4dfeac53864d32503a89d59a4204
swordcube <49110074+swordcube@users.noreply.github.com>
2025-05-09 11:22:44 -0500

    make sure fps doesn't go too far past the cap

--- project/src/backend/sdl/SDLApplication.cpp
+++ project/src/backend/sdl/SDLApplication.cpp
@@ -910,6 +910,11 @@ namespace lime {
 		currentUpdate = getTime();
 
 		double dt = currentUpdate - lastUpdate;
+
+		double dtLimit = framePeriod * 4;
+		if (dt > dtLimit)
+			dt = dtLimit;
+		
 		nextUpdate += dt;
 
 		if(nextUpdate >= framePeriod) {
